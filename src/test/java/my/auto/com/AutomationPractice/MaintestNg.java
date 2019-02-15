bd06f59ce4ce95f3b050cce41a91cd469f4b52b5
SaurabDey <saurab.jeet@gmail.com>
2019-02-15 09:10:46 +0530

    Added Common Utill

--- src/test/java/my/auto/com/AutomationPractice/MaintestNg.java
+++ src/test/java/my/auto/com/AutomationPractice/MaintestNg.java
@@ -4,13 +4,21 @@ import org.testng.annotations.Test;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.MediaEntityModelProvider;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -21,65 +29,75 @@ public class MaintestNg {
 	WebDriver Driver;
 	ExtentReports extent;
 	ExtentTest test;
+
 	@Test
-	public void f() {
+	public void f() throws IOException {
 
-		SignUPPage sign = new SignUPPage(Driver);
-		sign.signUp();
+		CommonUtility common = new CommonUtility(Driver);
+		try {
+			SignUPPage sign = new SignUPPage(Driver);
+			sign.signUp();
+			test.pass("signUp method was successful");
+		} catch (Exception e) {
+			test.fail("error in signUp method ").addScreenCaptureFromPath(common.screenshot("problem1"));
+		}
 
-		RegistrationPage regis = new RegistrationPage(Driver);
-		regis.registration();
+		try {
+			RegistrationPage regis = new RegistrationPage(Driver);
+			regis.registration();
+			test.pass("registration method was successful");
+		} catch (Exception e) {
+			
+			MediaEntityModelProvider mediaModel = MediaEntityBuilder.createScreenCaptureFromPath(common.screenshot("newway")).build();
+			test.fail("error in registration method ", mediaModel);
+		}
 
 	}
 
 	@Parameters("browserName")
 	@BeforeTest
 	public void beforeTest(String value) {
-		
+
 		ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("Resource/ExtentReport.html");
+		htmlReporter.config().setTheme(Theme.DARK);
+		htmlReporter.config().setChartVisibilityOnOpen(true);
+		htmlReporter.setAppendExisting(true);
+
 		extent = new ExtentReports();
 		extent.attachReporter(htmlReporter);
 		test = extent.createTest("SaurabFirstTest");
-		test.pass("Started my test");
-		
-		
+		test.info("Started my test");
 
-		String browser=value;
-		
+		String browser = value;
+
 		if (browser.equalsIgnoreCase("Chrome")) {
 			System.setProperty("webdriver.chrome.driver", "Resource/chromedriver.exe");
 			Driver = new ChromeDriver();
-			test.pass("chrome started");
-			
-		} 
-		else if(browser.equalsIgnoreCase("Firefox"))
-		{
+			test.info("chrome started");
+
+		} else if (browser.equalsIgnoreCase("Firefox")) {
 			System.setProperty("webdriver.gecko.driver", "Resource/geckodriver.exe");
 			Driver = new FirefoxDriver();
-			test.pass("firefox started");
-		}
-		else if(browser.equalsIgnoreCase("IE"))
-		{
+			test.info("firefox started");
+		} else if (browser.equalsIgnoreCase("IE")) {
 			System.setProperty("webdriver.ie.driver", "Resource/IEDriverServer.exe");
 			Driver = new InternetExplorerDriver();
-			test.pass("IE started");
-		}
-		else {
+			test.info("IE started");
+		} else {
 			throw new RuntimeException("Please provide correct browser name");
 		}
-		
 
 		Driver.manage().window().maximize();
-		test.pass("browser maximised");
+		test.info("browser maximised");
 
 		Driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		Driver.get("http://automationpractice.com/index.php?controller=authentication&back=my-account");
-		test.pass("URL successfully opened");
+		test.info("URL successfully opened");
 	}
 
 	@AfterTest
 	public void afterTest() {
-		test.pass("Execution over");
+		test.info("Execution over");
 		Driver.quit();
 		extent.flush();
 	}
