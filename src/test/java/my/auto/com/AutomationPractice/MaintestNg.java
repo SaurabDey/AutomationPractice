c43ba8f4e7b3cec8af6ac23c4f5153ea4817e23c
nachipriya14 <gadad.supriya@gmail.com>
2019-02-19 08:32:29 +0530

    gm

--- src/test/java/my/auto/com/AutomationPractice/MaintestNg.java
+++ src/test/java/my/auto/com/AutomationPractice/MaintestNg.java
@@ -1,24 +1,11 @@
 package my.auto.com.AutomationPractice;
 
 import org.testng.annotations.Test;
-
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.MediaEntityBuilder;
-import com.aventstack.extentreports.MediaEntityModelProvider;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
-import com.aventstack.extentreports.reporter.configuration.Theme;
-
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -27,30 +14,15 @@ import org.testng.annotations.AfterTest;
 
 public class MaintestNg {
 	WebDriver Driver;
-	ExtentReports extent;
-	ExtentTest test;
 
 	@Test
-	public void f() throws IOException {
+	public void f() {
 
-		CommonUtility common = new CommonUtility(Driver);
-		try {
-			SignUPPage sign = new SignUPPage(Driver);
-			sign.signUp();
-			test.pass("signUp method was successful");
-		} catch (Exception e) {
-			test.fail("error in signUp method ").addScreenCaptureFromPath(common.screenshot("problem1"));
-		}
+		SignUPPage sign = new SignUPPage(Driver);
+		sign.signUp();
 
-		try {
-			RegistrationPage regis = new RegistrationPage(Driver);
-			regis.registration();
-			test.pass("registration method was successful");
-		} catch (Exception e) {
-			
-			MediaEntityModelProvider mediaModel = MediaEntityBuilder.createScreenCaptureFromPath(common.screenshot("newway")).build();
-			test.fail("error in registration method ", mediaModel);
-		}
+		RegistrationPage regis = new RegistrationPage(Driver);
+		regis.registration();
 
 	}
 
@@ -58,48 +30,37 @@ public class MaintestNg {
 	@BeforeTest
 	public void beforeTest(String value) {
 
-		ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("Resource/ExtentReport.html");
-		htmlReporter.config().setTheme(Theme.DARK);
-		htmlReporter.config().setChartVisibilityOnOpen(true);
-		htmlReporter.setAppendExisting(true);
-
-		extent = new ExtentReports();
-		extent.attachReporter(htmlReporter);
-		test = extent.createTest("SaurabFirstTest");
-		test.info("Started my test");
-
-		String browser = value;
-
+		String browser=value;
+		
 		if (browser.equalsIgnoreCase("Chrome")) {
 			System.setProperty("webdriver.chrome.driver", "Resource/chromedriver.exe");
 			Driver = new ChromeDriver();
-			test.info("chrome started");
-
-		} else if (browser.equalsIgnoreCase("Firefox")) {
+		} 
+		else if(browser.equalsIgnoreCase("Firefox"))
+		{
 			System.setProperty("webdriver.gecko.driver", "Resource/geckodriver.exe");
 			Driver = new FirefoxDriver();
-			test.info("firefox started");
-		} else if (browser.equalsIgnoreCase("IE")) {
+		}
+		else if(browser.equalsIgnoreCase("IE"))
+		{
 			System.setProperty("webdriver.ie.driver", "Resource/IEDriverServer.exe");
 			Driver = new InternetExplorerDriver();
-			test.info("IE started");
-		} else {
+		}
+		else {
 			throw new RuntimeException("Please provide correct browser name");
 		}
+		
 
 		Driver.manage().window().maximize();
-		test.info("browser maximised");
 
 		Driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		Driver.get("http://automationpractice.com/index.php?controller=authentication&back=my-account");
-		test.info("URL successfully opened");
 	}
 
 	@AfterTest
 	public void afterTest() {
-		test.info("Execution over");
+		System.out.println("Close Page");
 		Driver.quit();
-		extent.flush();
 	}
 
 }
